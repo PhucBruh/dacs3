a725a008a4e3c83192829868df5ab8456322ee51
hoangtri2306 <hoangtri230604@gmail.com>
2024-05-16 21:48:06 +0700

    admin

---
+++ app/src/main/java/com/triphuc22ad/shoesshop/presentation/admin/components/DashboardScreen.kt
@@ -0,0 +1,9 @@
+package com.triphuc22ad.shoesshop.presentation.admin.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DashboardScreen() {
+    Text("Dashboard Screen")
+}
---
+++ app/src/main/java/com/triphuc22ad/shoesshop/presentation/admin/components/ProductsScreen.kt
@@ -0,0 +1,9 @@
+package com.triphuc22ad.shoesshop.presentation.admin.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProductsScreen() {
+    Text("Products Screen")
+}
